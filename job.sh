93d7dbb457ab6ec95d61a39d18e5c18e1d3665cf
zeterPL <ukaszjanowicz@gmail.com>
2025-01-17 10:41:20 +0100

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=quicksort_test       # Nazwa zadania
+#SBATCH --output=output_%j.txt          # Plik wyjściowy (logi)
+#SBATCH --error=error_%j.txt            # Plik błędów
+#SBATCH --ntasks=1                      # Liczba zadań
+#SBATCH --cpus-per-task=8               # Liczba rdzeni na zadanie
+
+# Uruchomienie programu z srun
+srun ./main
