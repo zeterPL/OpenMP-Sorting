8c9e520017ece0da7f3420db46402881bb164a3b
Piotr Szumowski <83671265+rzymski@users.noreply.github.com>
2025-01-17 12:04:31 +0100

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -22,11 +22,12 @@ void printX(int *vector, int x)
     {
         cout << vector[i] << ", ";
     }
+    cout << "\n";
 }
 
 void test_scalability()
 {
-    const vector<long> sizes = {100000, 1000000, 10000000};
+    const vector<long> sizes = {100000, 1000000, 10000000, 100000000, 1000000000};
 
     for (long N : sizes)
     {
@@ -37,39 +38,46 @@ void test_scalability()
         cout << "Testing N = " << N << "\n";
 
         // Sekwencyjny QuickSort
+        //printX(copy.data(), 10);
         auto start = chrono::high_resolution_clock::now();
         quicksort_serial(copy.data(), 0, N - 1);
         auto end = chrono::high_resolution_clock::now();
         chrono::duration<double> seq_quicksort_time = end - start;
         cout << "Sequential QuickSort time: " << seq_quicksort_time.count() << " seconds\n";
-        printX(copy.data(), 10);
+        //printX(copy.data(), 10);
 
         // Równoległy QuickSort
         copy = data;
+        //printX(copy.data(), 10);
         start = chrono::high_resolution_clock::now();
         quicksort_parallel_wrapper(copy.data(), N);
         end = chrono::high_resolution_clock::now();
         chrono::duration<double> par_quicksort_time = end - start;
         cout << "Parallel QuickSort time: " << par_quicksort_time.count() << " seconds\n";
+        //printX(copy.data(), 10);
 
         cout << "QuickSort Speedup: " << seq_quicksort_time.count() / par_quicksort_time.count() << "\n\n";
 
         // Sekwencyjny MergeSort
         copy = data;
+        //printX(copy.data(), 10);
         vector<int> temp(N);
         start = chrono::high_resolution_clock::now();
         mergesort_serial(copy.data(), temp.data(), 0, N - 1);
         end = chrono::high_resolution_clock::now();
         chrono::duration<double> seq_mergesort_time = end - start;
         cout << "Sequential MergeSort time: " << seq_mergesort_time.count() << " seconds\n";
+        //printX(copy.data(), 10);
 
         // Równoległy MergeSort
         copy = data;
+        //printX(copy.data(), 10);
         start = chrono::high_resolution_clock::now();
         mergesort_parallel_wrapper(copy.data(), N);
         end = chrono::high_resolution_clock::now();
         chrono::duration<double> par_mergesort_time = end - start;
         cout << "Parallel MergeSort time: " << par_mergesort_time.count() << " seconds\n";
+        //printX(copy.data(), 10);
 
         cout << "MergeSort Speedup: " << seq_mergesort_time.count() / par_mergesort_time.count() << "\n\n";
     }
