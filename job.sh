008ea03faac65ed3c548ed8c48a5e627d0cb6243
zeterPL <ukaszjanowicz@gmail.com>
2025-01-17 11:48:34 +0100

    change CPU's

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=output_%j.txt          # Plik wyjściowy (logi)
 #SBATCH --error=error_%j.txt            # Plik błędów
 #SBATCH --ntasks=1                      # Liczba zadań
-#SBATCH --cpus-per-task=8               # Liczba rdzeni na zadanie
+#SBATCH --cpus-per-task=6              # Liczba rdzeni na zadanie
 
 # Uruchomienie programu z srun
 srun ./main
